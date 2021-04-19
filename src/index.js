5538302ebaecbac728fcb51535ddd7206d4941a2
Tony Xu <Design.TXU@gmail.com>
2021-04-19 12:38:16 +1000

    Testing if I can push to forked repo after url origin change

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ import './styles.css'
 import codegen from 'codegen.macro'
 
 codegen`module.exports = require('@kentcdodds/react-workshop-app/codegen')`
+
+//commit note to test if I can push to forked repo
